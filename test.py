592bcd2ff7ce7fee033372e163a888696734a853
zhangyongkang <785621024@qq.com>
2026-05-08 18:01:23 +0800

    AI智能助手

--- test.py
+++ test.py
@@ -1,7 +1,61 @@
 import streamlit as st
+import os
+from openai import OpenAI
 
-st.title("hello")
-st.header("一级标题")
-st.subheader("二级标题")
-st.write("段落文字")
-st.image("./resources/banner.png")
+#设置页面的配置项
+st.set_page_config(
+    page_title="AI智能助手",
+    page_icon="🦝",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={}
+)
+
+#大标题
+st.title("AI智能助手")
+#logo
+st.logo("resources/banner.png")
+
+#系统提示词
+system_prompt = "You are a helpful assistant"
+
+#初始化聊天信息
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+#展示聊天信息
+for message in st.session_state.messages:
+    # if message["role"] == "user":
+    #     st.chat_message("user").write(message["content"])
+    # else:
+    #     st.chat_message("assistant").write(message["content"])
+    st.chat_message(message["role"]).write(message["content"])
+
+
+# 从 secrets 里取密钥
+#client = OpenAI(api_key=st.secrets["DEEPSEEK_API_KEY"],base_url=st.secrets["DEEPSEEK_API_URL"])
+#从环境变量里获取密钥
+client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"),base_url=os.getenv("DEEPSEEK_API_URL"))
+
+#聊天框
+prompt = st.chat_input("请输入你的问题")
+if prompt:
+    st.chat_message("user").write(prompt)
+    st.session_state.message.append({"role": "user", "content": prompt})
+
+
+    #调用大模型
+    response = client.chat.completions.create(
+    model="deepseek-v4-pro",
+    messages=[
+        {"role": "system", "content":system_prompt},
+        *st.session_state.messages,
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}}
+    )
+
+    st.chat_message("assistant").write(response.choices[0].message.content)
+    #保存大模型返回的结果
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
